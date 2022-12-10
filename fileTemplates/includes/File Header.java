c8ace3414e9b2e1151b5fa0d0f49aadede735097
愆凡 <zgwmail@icloud.com>
2022-12-10 14:34:05 +0800

    IU-2022.3 <snow@qianfande-iMac.lan Create easyJavadoc.xml, IntelliLang.xml, mybatisx.xml, project.default.xml, diff.xml, Interface.java, Default.xml, visualizationTool.xml, find.xml, dataSources.xml, File Header.java, androidEditors.xml, Default.xml, nodejs.xml, scala.xml, codeglance2.xml, maven.wrapper.mapping.xml, keymap.xml, editor.xml, debugger.xml, a-file-icons.xml, updates.xml, baseRefactoring.xml, ide.general.xml, rainbow_brackets.xml, advancedSettings.xml, shared-indexes.xml, ignored-suggested-plugins.xml, ui.lnf.xml, log_highlighting.xml, DontShowAgainFeedbackService.xml, filetypes.xml, remote-servers.xml, Java.xml, editor-font.xml, colors.scheme.xml, notifications.xml, scala_config.xml, databaseDrivers.xml, github.xml, templates.xml, laf.xml, customization.xml, sshConfigs.xml, Dracula Colorful -    AtomOneDark.icls, go.sdk.xml, jdk.table.xml, zoolytic.xml, acceptedLanguageLevels.xml, settingsSync.xml, proxy.settings.xml, vcs.xml, grazie_global.xml, macOS.xml, usageView.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,7 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 愆凡
+ * @date ${DATE} ${TIME}
+ */
+@Slf4j
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+ * @author 愆凡
+ * @date ${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
